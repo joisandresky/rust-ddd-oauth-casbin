8801a07290d7550b4d4d5a7968d5dc342d6b83b3
Jois Andresky <andreskyjois8@gmail.com>
2024-09-14 11:00:07 +0700

    cache permissions with redis

--- src/application/services/redis_svc.rs
+++ src/application/services/redis_svc.rs
@@ -1,6 +1,8 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
-use crate::domain::repositories::redis_repo::RedisRepository;
+use crate::{
+    domain::repositories::redis_repo::RedisRepository, infra::errors::app_error::AppError,
+};
 
 #[derive(Clone)]
 pub struct RedisService<R> {
@@ -13,5 +15,24 @@ where
 {
     pub fn new(redis_repo: Arc<R>) -> Self {
         Self { redis_repo }
+    }
+
+    pub async fn get_permissions_list(&self) -> Result<HashMap<String, Vec<String>>, AppError> {
+        let permissions_str = self.redis_repo.get_value("permissions").await?;
+        let permissions: HashMap<String, Vec<String>> = serde_json::from_str(&permissions_str)?;
+
+        Ok(permissions)
+    }
+
+    pub async fn set_permissions_list(
+        &self,
+        permissions: HashMap<String, Vec<String>>,
+    ) -> Result<(), AppError> {
+        let permissions_json = serde_json::to_string(&permissions)?;
+        self.redis_repo
+            .set_value_with_expiry("permissions", &permissions_json, 3600)
+            .await?;
+
+        Ok(())
     }
 }
--- src/interface/api/permission_handler.rs
+++ src/interface/api/permission_handler.rs
@@ -1,19 +1,34 @@
 use std::{collections::HashMap, sync::Arc};
 
-use axum::{Json, Router};
+use axum::{extract::State, Json, Router};
 use tokio::fs;
 
-use crate::{application::state::AppState, infra::errors::app_error::AppError};
+use crate::{
+    application::state::AppState,
+    infra::{errors::app_error::AppError, utils::response::SuccessResponse},
+};
 
 pub fn setup_permission_handler() -> Router<Arc<AppState>> {
     Router::new().route("/list", axum::routing::get(get_permission_list))
 }
 
 // TODO: change into caching or changes detection when performance issue happens
-async fn get_permission_list() -> Result<Json<HashMap<String, Vec<String>>>, AppError> {
+async fn get_permission_list(
+    State(app_state): State<Arc<AppState>>,
+) -> Result<SuccessResponse<HashMap<String, Vec<String>>>, AppError> {
+    if let Ok(permissions) = app_state.svc.redis.get_permissions_list().await {
+        return Ok(SuccessResponse::with_data(200, permissions));
+    }
+
     let permission_list = fs::read_to_string("etc/permissions.json").await?;
 
     let permission_list: HashMap<String, Vec<String>> = serde_json::from_str(&permission_list)?;
 
-    Ok(Json(permission_list))
+    app_state
+        .svc
+        .redis
+        .set_permissions_list(permission_list.clone())
+        .await?;
+
+    Ok(SuccessResponse::with_data(200, permission_list))
 }
